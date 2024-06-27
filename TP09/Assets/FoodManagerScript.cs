ce5961268df0e5a0d75052faa26e7b19e23db28e
ARIEL CHOLAKIAN SAZ <48387289@est.ort.edu.ar>
2024-06-27 10:06:37 -0300

    Did damage control

---
+++ TP09/Assets/FoodManagerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodManagerScript : MonoBehaviour
+{
+    public GameObject[] Comidas;
+    int Comida1, Comida2, OpcionCorrecta;
+    int[] Opciones = { 0,0,0 };
+    private void Start()
+    {
+        Comida1 = Random.Range(0, 16); Comida2 = Random.Range(0, 16); OpcionCorrecta = Random.Range(0, 2);
+        while (Opciones[0] == Opciones[1] || Opciones[0] == Opciones[2] || Opciones[1] == Opciones[2] || Opciones[1] == Comida2 || Opciones[2] == Comida2 || Opciones[0] == Comida2)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Opciones[i] = Random.Range(0, 16);
+            }
+        }
+        Opciones[OpcionCorrecta] = Comida2;
+    }
+
+}
--- TP09/Assets/Prefabs/Producto.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Producto : MonoBehaviour
-{
-    public int precio;
-}
